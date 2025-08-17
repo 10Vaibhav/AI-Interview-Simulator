5d7fc703b82882f372d56c517f777ad8194e3393
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-08-17 14:04:32 +0530

    Enhancing user interaction with Session State

--- app.py
+++ app.py
@@ -5,72 +5,105 @@ st.set_page_config(page_title="AI Interview Simulator", page_icon="💬")
 
 st.title("AI Interview Simulator!")
 
-# Build the Setup page
-st.subheader("Personal Information", divider="rainbow")
+if "setup_complete" not in st.session_state:
+    st.session_state.setup_complete = False
 
-name = st.text_input(label="Name", max_chars=None, placeholder="Enter your name")
+def complete_setup():
+    st.session_state.setup_complete = True
 
-experience = st.text_area(label = "Experience", value="", height=None, max_chars=None, placeholder="Describe your experience")
+if not st.session_state.setup_complete:
+    # Build the Setup page
+    st.subheader("Personal Information", divider="rainbow")
 
-skills = st.text_area(label = "Skills", value="", height=None, max_chars = None, placeholder= "List your Skills")
+    if "name" not in st.session_state:
+        st.session_state["name"] = ""
+    if "experience" not in st.session_state:
+        st.session_state["experience"] = ""
+    if "skills" not in st.session_state:
+        st.session_state["skills"] = ""
 
-st.write(f"**Your Name**: {name}")
-st.write(f"**Your Experience**: {experience}")
-st.write(f"**Your Skills**: {skills}")
+    st.session_state["name"] = st.text_input(label="Name", max_chars=None, value= st.session_state["name"],placeholder="Enter your name")
 
-st.subheader("Company and Position", divider = "rainbow")
+    st.session_state["experience"] = st.text_area(label = "Experience", value=st.session_state["experience"], height=None, max_chars=None, placeholder="Describe your experience")
 
-col1, col2 = st.columns(2)
+    st.session_state["skills"] = st.text_area(label = "Skills", value="", height=None ,max_chars = None, placeholder= "List your Skills")
 
-with col1:
-    level = st.radio(
-        "Choose level",
-        key = "visibility",
-        options = ["Junior", "Mid-level", "Senior"]
-    )
+    st.write(f"**Your Name**: {st.session_state["name"]}")
+    st.write(f"**Your Experience**: {st.session_state["experience"]}")
+    st.write(f"**Your Skills**: {st.session_state["skills"]}")
 
-with col2:
-    position = st.selectbox(
-        "Choose a position",
-        ("Data Scientist", "Data engineer", "ML Engineer", "BI Analyst", "Financial Analyst")
-    )
+    st.subheader("Company and Position", divider = "rainbow")
 
-company = st.selectbox(
-    "Choose a Company",
-    ("Amazon", "Meta", "Udemy", "365 Company", "Nestle", "Linkedin", "Spotify")
-)
+    if "level" not in st.session_state:
+        st.session_state["level"] = "Junior"
+    if "position" not in st.session_state:
+        st.session_state["position"] = "Data Scientist"
+    if "company" not in st.session_state:
+        st.session_state["company"] = "Amazon"
 
-st.write(f"**Your Information**: {level} {position} at {company}")
+    col1, col2 = st.columns(2)
 
-# Initializing OpenAI Client
-client = OpenAI(api_key= st.secrets["OPENAI_API_KEY"])
-
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4o"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "system", "content": f"You are an HR executive that interviews an interviewee called {name} with experience {experience} and skills {skills}. You should interview them for the position {level} {position} at the company {company}"}]
-
-# Display the chat messages.
-for message in st.session_state.messages:
-    if message["role"] != "system":
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-# Implementing the Chat Functionality
-if prompt := st.chat_input("Your answer."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model= st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]} for m in st.session_state.messages
-            ],
-            stream=True,
+    with col1:
+        st.session_state["level"] = st.radio(
+            "Choose level",
+            key = "visibility",
+            options = ["Junior", "Mid-level", "Senior"]
         )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
+    with col2:
+        st.session_state["position"] = st.selectbox(
+            "Choose a position",
+            ("Data Scientist", "Data engineer", "ML Engineer", "BI Analyst", "Financial Analyst")
+        )
+
+    st.session_state["company"] = st.selectbox(
+        "Choose a Company",
+        ("Amazon", "Meta", "Udemy", "365 Company", "Nestle", "Linkedin", "Spotify")
+    )
+
+    st.write(f"**Your Information**: {st.session_state["level"]} {st.session_state["position"]} at {st.session_state["company"]}")
+
+    if st.button("Start Interview", on_click=complete_setup):
+        st.write("Setup Complete. Starting interview...")
+
+if st.session_state.setup_complete:
+
+    st.info(
+        """
+        Start By Introducing Yourself.
+        """,
+        icon="👋"
+    )
+
+    # Initializing OpenAI Client
+    client = OpenAI(api_key= st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "system", "content": f"You are an HR executive that interviews an interviewee called {st.session_state["name"]} with experience {st.session_state["experience"]} and skills {st.session_state["skills"]}. You should interview them for the position {st.session_state["level"]} {st.session_state["position"]} at the company {st.session_state["company"]}"}]
+
+    # Display the chat messages.
+    for message in st.session_state.messages:
+        if message["role"] != "system":
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    # Implementing the Chat Functionality
+    if prompt := st.chat_input("Your answer."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model= st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]} for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
