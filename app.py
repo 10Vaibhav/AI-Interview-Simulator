c6ec8ecb99881cbd12bf59e2c6507a9a29af57b6
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-08-17 12:35:29 +0530

    Build the setup page

--- app.py
+++ app.py
@@ -5,6 +5,43 @@ st.set_page_config(page_title="AI Interview Simulator", page_icon="💬")
 
 st.title("AI Interview Simulator!")
 
+# Build the Setup page
+st.subheader("Personal Information", divider="rainbow")
+
+name = st.text_input(label="Name", max_chars=None, placeholder="Enter your name")
+
+experience = st.text_area(label = "Experience", value="", height=None, max_chars=None, placeholder="Describe your experience")
+
+skills = st.text_area(label = "Skills", value="", height=None, max_chars = None, placeholder= "List your Skills")
+
+st.write(f"**Your Name**: {name}")
+st.write(f"**Your Experience**: {experience}")
+st.write(f"**Your Skills**: {skills}")
+
+st.subheader("Company and Position", divider = "rainbow")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    level = st.radio(
+        "Choose level",
+        key = "visibility",
+        options = ["Junior", "Mid-level", "Senior"]
+    )
+
+with col2:
+    position = st.selectbox(
+        "Choose a position",
+        ("Data Scientist", "Data engineer", "ML Engineer", "BI Analyst", "Financial Analyst")
+    )
+
+company = st.selectbox(
+    "Choose a Company",
+    ("Amazon", "Meta", "Udemy", "365 Company", "Nestle", "Linkedin", "Spotify")
+)
+
+st.write(f"**Your Information**: {level} {position} at {company}")
+
 # Initializing OpenAI Client
 client = OpenAI(api_key= st.secrets["OPENAI_API_KEY"])
 
@@ -12,7 +49,7 @@ if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "system", "content": "You are a helpful tool that talks like a pirate."}]
+    st.session_state.messages = [{"role": "system", "content": f"You are an HR executive that interviews an interviewee called {name} with experience {experience} and skills {skills}. You should interview them for the position {level} {position} at the company {company}"}]
 
 # Display the chat messages.
 for message in st.session_state.messages:
@@ -36,3 +73,4 @@ if prompt := st.chat_input("Your answer."):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+
